80851920d7104d6a57a6bb96b79b63f9f6af4aa0
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-04-12 13:44:30 -0700

    built todo reducer and state

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from "../features/counter/todo/todoSlice";
+
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    todos: todosReducer,
   },
 });
--- src/features/counter/todo/Todo.js
+++ src/features/counter/todo/Todo.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { create, edit, remove, toggleComplete } from "./todoSlice";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  const todos = useSelector((state) => state.todos);
+  const [inputText, setInputText] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(create(inputText));
+    setInputText("");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+        />
+
+        <button type="submit">Create New</button>
+      </form>
+    </div>
+  );
 };
 
 export default Todo;
